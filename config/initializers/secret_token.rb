4321a7bf3240fb9a270428ec89219de6108e116f
Jay Snee <jay.snee@me.com>
2014-02-26 17:55:59 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PaypalTest::Application.config.secret_key_base = 'b1ae1594fb1d84e16b868a24535828379364a49d7945abbc8f8e8b778fb6cc349eb6cff6b1ad738a7c751b9f814f6574cd2738f3a9aafe811bd4d41333d8e6dd'
